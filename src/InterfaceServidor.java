78053e497096708fb49c2cf03cb70463c4046c81
1mip1 <inesreispardal@gmail.com>
2021-05-03 02:22:54 +0100

    iuiui

---
+++ src/InterfaceServidor.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+// funçoes que queres usar no servidor, isto serve para o callback
+public interface InterfaceServidor extends java.rmi.Remote{
+
+
+}
